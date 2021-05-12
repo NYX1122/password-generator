890c64791360826f228072be0f881bc6961697cf
BoDee <nyx0hemir@gmail.com>
2021-05-12 03:18:34 -0600

    forgot to commit, finished now

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+//objects and variables
 var passwordObj = {
   length: 0,
   lowercase: false,
@@ -7,23 +7,49 @@ var passwordObj = {
   special: false
 };
 
-var generateBtn = document.querySelector("#generate");
+var characterObj = {
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numeric: "0123456789",
+  special: " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+};
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var password = "";
 
-  passwordText.value = password;
 
+//handy functions
+var decider = function() {
+  var random = Math.floor(Math.random() * 4);
+  return random;
 }
-
 
 var passwordPrompts = function() {
   passwordLength();
   console.log(passwordObj.length);
+  booleanPasswordPrompts();
 };
 
+var booleanPasswordPrompts = function(){
+  passwordLowercase();
+  console.log(passwordObj.lowercase);
+
+  passwordUppercase();
+  console.log(passwordObj.uppercase);
+
+  passwordNumeric();
+  console.log(passwordObj.numeric);
+
+  passwordSpecial();
+  console.log(passwordObj.special);
+
+  if(!passwordObj.lowercase && !passwordObj.uppercase && !passwordObj.numeric && !passwordObj.special) {
+    window.alert("You're really looking for bugs aren't you? Try again XD")
+    booleanPasswordPrompts();
+  };
+};
+
+
+//prompts
 var passwordLength = function () {
   passwordObj.length = window.prompt("Length of password?");
   passwordObj.length = parseInt(passwordObj.length);
@@ -34,12 +60,93 @@ var passwordLength = function () {
   else if(passwordObj.length < 8 || passwordObj.length > 128) {
     window.alert("Please enter a number between 8 and 128");
     passwordLength();
+  };
+};
+
+var passwordLowercase = function () {
+  passwordObj.lowercase = window.confirm("Lowercase letters?");
+};
+
+var passwordUppercase = function () {
+  passwordObj.uppercase = window.confirm("Uppercase letters?");
+};
+
+var passwordNumeric = function () {
+  passwordObj.numeric = window.confirm("Numeric characters?");
+};
+
+var passwordSpecial = function () {
+  passwordObj.special = window.confirm("Special characters?");
+};
+
+
+//initializing function
+function writePassword() {
+  password = "";
+  generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+};
+
+
+//randomizers
+var lowercaseRandomizer = function () {
+  if(passwordObj.lowercase) {
+    password += characterObj.lowercase.charAt(Math.floor(Math.random() * characterObj.lowercase.length));
+  }
+  else {
+  deciderFunctionArray[decider()]();
+  }
+};
+
+var uppercaseRandomizer = function () {
+  if(passwordObj.uppercase) {
+    password += characterObj.uppercase.charAt(Math.floor(Math.random() * characterObj.uppercase.length));
+  }
+  else {
+    deciderFunctionArray[decider()]();
   }
 }
 
-var generatePassword = function() {
-  passwordPrompts();
+var numericRandomizer = function () {
+  if(passwordObj.numeric) {
+    password += characterObj.numeric.charAt(Math.floor(Math.random() * characterObj.numeric.length));
+  }
+  else {
+    deciderFunctionArray[decider()]();
+  }
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); 
+var specialRandomizer = function () {
+  if(passwordObj.special) {
+    password += characterObj.special.charAt(Math.floor(Math.random() * characterObj.special.length));
+  }
+  else {
+    deciderFunctionArray[decider()]();
+  }
+};
+
+
+//randomizer array
+var deciderFunctionArray = [
+  lowercaseRandomizer,
+  uppercaseRandomizer,
+  numericRandomizer,
+  specialRandomizer
+];
+
+
+//loop
+var generatePassword = function() {
+  passwordPrompts();
+  for(i = 0; i < passwordObj.length; i++) {
+    debugger;
+    deciderFunctionArray[decider()]();
+    console.log(password);
+  };
+};
+
+
+//event listener
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
